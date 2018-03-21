ae20c110a536227645287c84948d8f5357571ce0
Nagy Patrik <npatrik11@gmail.com>
2018-03-21 13:57:43 +0100

    Edit dog api modified

--- middleware/api/editDog.js
+++ middleware/api/editDog.js
@@ -12,22 +12,6 @@ module.exports = function () {
             sex: req.body.sex
         };
 
-        if (dogFromRequest.name.length === 0) {
-            return res.send(422).end('Name is not filled');
-        }
-
-        if (dogFromRequest.type.length === 0) {
-            return res.send(422).end('Type is not filled')
-        }
-
-        if (dogFromRequest.age.length === 0) {
-            return res.send(422).end('Age is not filled')
-        }
-
-        if (dogFromRequest.sex.length === 0) {
-            return res.send(422).end('Sex is not filled')
-        }
-
         Dog.update({_id: req.params.dogid}, {$set: dogFromRequest}, function () {
             return res.send(200);
         });
